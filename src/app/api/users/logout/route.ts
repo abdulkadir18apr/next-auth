eb67dfda6860e7b977a908e1f0c9a7110c15e39b
abdulkadir18apr <114944605+abdulkadir18apr@users.noreply.github.com>
2024-03-20 23:07:43 +0530

    moified logout

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -3,7 +3,7 @@ import {NextResponse} from "next/server";
 export async function GET(){
     try{
         const response=NextResponse.json({message:"logout Successfully",success:true});
-        response.cookies.set("token","",{httpOnly:true,expires:new Date(0)});
+        response.cookies.set("token","",{httpOnly:true});
         return response;
     }catch(err:any){
         return NextResponse.json({error:err.message},{status:500})
